0ee7484098d50c7b012e0964a1ed1f749bd1fd9e
jaehunshin <jhshin0422@gmail.com>
2026-05-27 00:14:42 +0900

    feat: Redis GEO 기반 주변 카페 조회 구현
    - Redis GEOADD로 카페 위치 저장
    - GEOSEARCH로 반경 내 카페 거리순 조회
    - 조회 결과를 카페명과 거리 정보로 반환

--- src/main/java/com/example/redisintermediateproject/geospatial/CafeService.java
+++ src/main/java/com/example/redisintermediateproject/geospatial/CafeService.java
@@ -1,18 +1,55 @@
 package com.example.redisintermediateproject.geospatial;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CafeService {
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String GEOSPATIAL_KEY = "cafe";
 
     public void addCafe(AddCafeRequestDto addCafeRequestDto) {
-        return;
+        // GEOADD [key] [lon] [lat] member
+        redisTemplate.opsForGeo()
+                .add(
+                        GEOSPATIAL_KEY,
+                        new Point(
+                                addCafeRequestDto.getLongitude(),
+                                addCafeRequestDto.getLatitude()),
+                        addCafeRequestDto.getName()
+                );
     }
 
     public List<Object> findCafesNearby(double longitude, double latitude, double distanceKm) {
-        return null;
+
+        RedisGeoCommands.GeoSearchCommandArgs args = RedisGeoCommands.GeoSearchCommandArgs
+                .newGeoSearchArgs()
+                .includeDistance()
+                .sortAscending();
+
+        // GEOSEARCH [key] FROMLONLAT [lon] [lat] BYRADIUS [거리] [단위] ASC WITHDIST
+        return redisTemplate.opsForGeo().search(
+                        GEOSPATIAL_KEY,
+                        GeoReference.fromCoordinate(new Point(longitude, latitude)),
+                        new Distance(distanceKm, Metrics.KILOMETERS),
+                        args
+                ).getContent().stream()
+                .map(geoLocationGeoResult -> new CafeDistance(
+                        geoLocationGeoResult.getContent().getName(),
+                        geoLocationGeoResult.getDistance().getValue()
+                ))
+                .collect(Collectors.toList());
     }
+
 }
